47a98ee1b41397bd84d4fb4c1210ae4ac55a682e
Giuseppe <giusscos@icloud.com>
2026-08-08 15:38:43 +0200

    Initial Commit

---
+++ Stor/StorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorApp.swift
+//  Stor
+//
+//  Created by Giuseppe Cosenza on 08/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct StorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
